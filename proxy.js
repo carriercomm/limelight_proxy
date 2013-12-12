86ac16cc7ed2e6116b22bbc6be31a758239be8d5
Travis Tidwell <travist349@gmail.com>
2013-12-11 21:26:50 -0600

    Fixing this library to use limelight.js

--- proxy.js
+++ proxy.js
@@ -1,56 +1,14 @@
 var http = require("http");
 var needle = require("needle");
-var crypto = require("crypto");
-var url = require("url");
 var _ = require("underscore");
 var request = require("request");
 var nconf = require('nconf');
+var limelight = require('limelight');
 
 // Load the configurations.
 nconf.argv().file({ file: 'config.json' });
-
-/**
- * Authenticate a Limelight request.
- *
- * @param type
- * @param url
- * @param params
- */
-var authenticate_request = function($http_verb, $resource_url, $params) {
-  $params = $params ? $params : {};
-  var $url = $resource_url + '?';
-  var $parsed_url = url.parse($resource_url);
-  var $str_to_sign = $http_verb + '|' + $parsed_url.host + '|' + $parsed_url.pathname + '|';
-  $str_to_sign = $str_to_sign.toLowerCase();
-  if (!$params.hasOwnProperty('expires')) {
-    $params.expires = Math.floor(new Date().getTime() / 1000) + 300;
-  }
-  $params.access_key = nconf.get('access_key');
-
-  // Get all the keys in an array.
-  var keys = [], key = '', len = 0, i = 0;
-  for (key in $params) {
-    if ($params.hasOwnProperty(key)) {
-      keys.push(key);
-    }
-  }
-
-  // Sort the keys...
-  keys.sort();
-  len = keys.length;
-
-  // Iterate through all of the sorted keys.
-  for (i = 0; i < len; i++) {
-    key = keys[i];
-    $str_to_sign += key + '=' + $params[key] + '&';
-    $url += encodeURIComponent(key) + '=' + encodeURIComponent($params[key]) + '&';
-  }
-
-  $str_to_sign = $str_to_sign.substring(0, $str_to_sign.length - 1);
-  var $signature = crypto.createHmac("SHA256", nconf.get('secret')).update($str_to_sign).digest('base64');
-  $url += 'signature=' + encodeURIComponent($signature);
-  return $url;
-};
+var access_key = nconf.get('access_key');
+var secret = nconf.get('secret');
 
 /**
  * Create a proxy server.
@@ -76,7 +34,7 @@ http.createServer(function (req, res) {
       var mediaId = result[1];
 
       // Get the encodings from this media.
-      var signed_url = authenticate_request("GET", base + mediaId + '/encodings.json');
+      var signed_url = limelight.authenticate("GET", base + mediaId + '/encodings.json', access_key, secret);
       needle.get(signed_url, function (error, response, info) {
 
         // Check to make sure the request is valid.
@@ -94,7 +52,7 @@ http.createServer(function (req, res) {
           if (info.media_type == 'Video') {
 
             // Get the download url for this media.
-            var downloadRequest = authenticate_request("POST", base + mediaId + '/download_url');
+            var downloadRequest = limelight.authenticate("POST", base + mediaId + '/download_url', access_key, secret);
 
             // The download qualities.
             var qualities = ['hd', 'high', 'medium', 'low'];
